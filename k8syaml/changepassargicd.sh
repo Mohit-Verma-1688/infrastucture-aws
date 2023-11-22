e8750189d2a13bd7340949e7856ab9c127b74253
mohit-verma-1688 <mohitverma160288@gmail.com>
2023-11-22 17:31:44 +0900

    restructure the folder

--- k8syaml/changepassargicd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PASS=$(kubectl -n argocd get secret argocd-initial-admin-secret --template={{.data.password}} | base64 -d; echo)
-NEWPASS=Newdelhi@15
-echo $NEWPASS  
-argocd login dev-argocd.themulticlouding.com --insecure --username admin --password $PASS
-argocd account update-password --current-password $PASS --new-password $NEWPASS
-#kubectl -n argocd delete secret argocd-initial-admin-secret
